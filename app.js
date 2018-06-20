b33b0ff4c038c280e0d39800c88bf0bf857043df
Mohammed alotaibi <muhammedalotaibi13@gmail.com>
2018-06-20 10:36:19 +0300

    add header and footer in sepreat file and include them

--- app.js
+++ app.js
@@ -3,25 +3,42 @@ var app = express();
 
 
 app.use(express.static(__dirname + "/public"));
+app.set("view engine" , "ejs");
 
 app.get("/" , function(req ,res){
-	res.render("landingPage.ejs");
+	res.render("landingPage");
 })
 
 app.get("/register" , function(req , res){
-   res.render("register.ejs");
+   res.render("register");
 });
 
 app.get("/signIn" , function(req ,res){
-	res.render("signInPage.ejs");
+	res.render("signInPage");
 });
 
 app.get("/message" , function(req , res){
-    res.render("sendToUs.ejs");
+    res.render("sendToUs");
 });
 
 app.get("/about" , function(req , res){
-   res.render("whoWeAre.ejs");
+   res.render("whoWeAre");
+});
+
+app.get("/pay" , function(req , res){
+   res.render("payPage");
+});
+
+app.get("/profile" , function(req , res){
+   res.render("profilePage");
+});
+
+app.get("/result" , function(req , res){
+  res.render("resultPage");
+});
+
+app.get("/test" , function(req , res){
+  res.render("testPage");
 });
 
 
