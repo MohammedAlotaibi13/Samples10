4a64ffbed493513ccdcfaf5142b41613f378bf41
Mohammed alotaibi <muhammedalotaibi13@gmail.com>
2018-09-21 17:30:22 +0300

    delete engine from package.json and reset public directory

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ var MongoStore = require('connect-mongo')(session);
 
 // connect mongo database
 mongoose.connect(process.env.DATABASE)
-
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 app.set("view engine" , "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
