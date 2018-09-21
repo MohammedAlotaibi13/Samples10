29d82a099cce05cf63e0a620f084a57b90e1a26e
Mohammed alotaibi <muhammedalotaibi13@gmail.com>
2018-09-21 16:55:53 +0300

    change publich directory of stylsheets

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var MongoStore = require('connect-mongo')(session);
 // connect mongo database
 mongoose.connect(process.env.DATABASE)
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine" , "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
