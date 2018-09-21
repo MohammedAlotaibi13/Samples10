b171a471e244e34dbb59419f0229819747b3577c
Mohammed alotaibi <muhammedalotaibi13@gmail.com>
2018-09-21 15:52:34 +0300

    change landing page directory to blog page

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var middleware = require("../middleware/index")
 
 router.get("/" , function(req ,res){
     
-	res.render("landingPage");
+	res.render("blog");
 })
 
 router.get("/about" , function(req , res){
