d8b5088c1e61fd50a8e0e1754243c84149806da6
Mohammed alotaibi <muhammedalotaibi13@gmail.com>
2020-02-23 21:20:56 +0300

    reupload new test platform

--- app.js
+++ app.js
@@ -31,8 +31,6 @@ mongoose.set( 'useUnifiedTopology', true )
 mongoose.set('useCreateIndex', true);
 mongoose.Promise = global.Promise
 mongoose.connect(process.env.DATABASE)
-
-
 mongoose.set('useFindAndModify', false);
 
 app.use(express.static("public"));
